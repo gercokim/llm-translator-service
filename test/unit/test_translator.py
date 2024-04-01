db755b2b25c25be4aea111962eb2aecf83fd13a5
gercokim <gckim@andrew.cmu.edu>
2024-04-01 13:03:53 -0400

    Ported modified mock tests from checkpoint 1

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,3 +1,5 @@
+import vertexai
+from mock import patch
 from src.translator import translate_content
 
 
@@ -11,3 +13,34 @@ def test_llm_normal_response():
 
 def test_llm_gibberish_response():
     pass
+
+@patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+def test_unexpected_language(mocker):
+  mocker.return_value.text = "I don't understand your request"
+  content = "Aquí está su primer ejemplo."
+  assert translate_content(content) == (True, content)
+
+@patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+def test_empty_response(mocker):
+  mocker.return_value = {}
+  content = "Aquí está su primer ejemplo."
+  assert translate_content(content) == (True, content)
+
+@patch('vertexai.preview.language_models._PreviewChatModel.start_chat')
+def test_broken_chat_session(mocker):
+  mocker.return_value = {}
+  content = "Aquí está su primer ejemplo."
+  assert translate_content(content) == (True, content)
+
+@patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+def test_malformed_response(mocker):
+  mocker.return_value.text = "Here is your first example."
+  del mocker.return_value.text
+  content = "Aquí está su primer ejemplo."
+  assert translate_content(content) == (True, content)
+
+@patch('vertexai.preview.language_models._PreviewChatModel.from_pretrained')
+def test_broken_model(mocker):
+  mocker.return_value = {}
+  content = "Aquí está su primer ejemplo."
+  assert translate_content(content) == (True, content)
