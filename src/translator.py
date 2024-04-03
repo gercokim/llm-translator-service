ff8eca6670f39ead30285adb5294ea4d785999de
gercokim <gckim@andrew.cmu.edu>
2024-04-03 17:59:16 -0400

    Made translate function more robust

--- src/translator.py
+++ src/translator.py
@@ -18,18 +18,18 @@ def translate_content(content: str) -> tuple[bool, str]:
             "temperature": 0.7,  # Temperature controls the degree of randomness in token selection.
             "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
         }
-    #try:
-    chat_model = ChatModel.from_pretrained("chat-bison@001")
-    chat = chat_model.start_chat(context=context1)
-    response1 = chat.send_message(content, **parameters)
-    chat = chat_model.start_chat(context=context2)
-    response2 = chat.send_message(content, **parameters)
-    b1 = "true" in response2.text or "True" in response2.text
-    b2 = "false" in response2.text or "False" in response2.text
-    if not b1 and not b2:
-        return (b1, "I was unable to answer your request")
-    return (b1, response1.text)
-    #except Exception as e:
-        #return (False, "I was unable to answer your request")
+    try:
+        chat_model = ChatModel.from_pretrained("chat-bison@001")
+        chat = chat_model.start_chat(context=context1)
+        response1 = chat.send_message(content, **parameters)
+        chat = chat_model.start_chat(context=context2)
+        response2 = chat.send_message(content, **parameters)
+        b1 = "true" in response2.text or "True" in response2.text
+        b2 = "false" in response2.text or "False" in response2.text
+        if not b1 and not b2:
+            return (b1, "I was unable to answer your request")
+        return (b1, response1.text)
+    except Exception as e:
+        return (False, "I was unable to answer your request")
 
 print(translate_content('Aquí está su primer ejemplo.'))
